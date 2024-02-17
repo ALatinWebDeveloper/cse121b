78ee1d7b8bd7f0eb000f6ae4736ae92f7ac1665b
Marcus <juandbeta@gmail.com>
2024-02-17 21:05:21 +0100

    fina project finished

--- scripts/finalProyect.js
+++ scripts/finalProyect.js
@@ -5,8 +5,22 @@ let mainBackground = document.querySelector("main");
 let pokedex = [];
 let link = "https://pokeapi.co/api/v2/pokemon/";
 
+/* Call the api for each element up to 151(the max number of pokemons on the first generation)
+    and stores the info in the pokedex variable, then calls the format data function*/
+
+const getPokemon = async () => {
+
+    for (let i = 1; i <= 151; i++) {
+
+        const respons = await fetch(link + i);
+        pokedex = await respons.json();
+
+        formatData(pokedex);
+    }
+}
 
 /*fixId fix the Id of the pokemon to be displayed with one or two "0" in order to have three digist (001, 010, 100) */
+
 function formatData(poke) {
 
     let pokeId = poke.id.toString();
@@ -23,6 +37,7 @@ function formatData(poke) {
     pokeDisplay(poke, pokeHeight, pokeWeight, pokeId);
 
 }
+
 
 
 const pokeDisplay = (poke, pokeHeight, pokeWeight, pokeId) => {
@@ -56,17 +71,6 @@ const pokeDisplay = (poke, pokeHeight, pokeWeight, pokeId) => {
         </div >
     `;
     pokeProfile.append(div);
-}
-
-const getPokemon = async () => {
-
-    for (let i = 1; i <= 151; i++) {
-
-        const respons = await fetch(link + i);
-        pokedex = await respons.json();
-
-        formatData(pokedex);
-    }
 }
 
 function clear() {
